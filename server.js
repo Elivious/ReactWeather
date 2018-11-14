d0b3faaf51d28f940c4e2dee6d3d9d91cd8ab14c
elivious <xamrucs74@gmail.com>
2018-11-14 12:13:22 -0800

    error fix

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var express = require('express');
 
 var app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-    if (req.headers['x-forward-proto'] === 'http') {
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
         next();
     } else {
-        res.redirect('http//' + req.hostname + req.url);
+        res.redirect('http://' + req.hostname + req.url);
     }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-    console.log('express server is up on port 3000' + PORT);
+    console.log('Express server is up on port ' + PORT);
 });
